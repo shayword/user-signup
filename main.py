6cbaed5ebda1ff20e45500288245c04f0b5447d0
Shay Throgmartin <Shay Throgmartin>
2017-01-30 14:52:19 -0800

    started work for the first page form

--- main.py
+++ main.py
@@ -15,10 +15,43 @@
 # limitations under the License.
 #
 import webapp2
+import cgi
+
+def build_page(textarea_content):
+    username_label = "<label>Username:</label>"
+    username_input = "<input type='text' name='username' />"
+
+    password_label = "<label>Password:</label>"
+    password_input = "<input type='text' name='password' />"
+
+    ver_pwd_label = "<label>Verify Password:</label>"
+    ver_pwd_input = "<input type='text' name='ver_pwd' />"
+
+    email_label = "<label>Email(optional):</label>"
+    email_input = "<input type='text' name='email' />"
+
+    submit = "<input type='submit' />"
+
+    form = ("<form method='post'>" +
+            username_label + username_input + "<br>" +
+            password_label + password_input + "<br>" +
+            ver_pwd_label + ver_pwd_input + "<br>" +
+            email_label + email_input + "<br>" +
+            submit + "</form>")
+
+    header = "<h1>User Signup</h1>"
+
+    return header + form
+
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        content = build_page("")
+        self.response.write(content)
+
+    def post(self):
+
+
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
