e345d421584e011fd9c8b660374073934ea640c6
Shay Throgmartin <Shay Throgmartin>
2017-02-02 00:07:49 -0600

    done

--- main.py
+++ main.py
@@ -16,48 +16,112 @@
 #
 import webapp2
 import cgi
+import re
 
-def build_page(textarea_content):
+def style_error(message):
+    return "<span style='color: red;'> " + message + "</span>"
+
+def build_page(postVars):
     username_label = "<label>Username:</label>"
-    username_input = "<input type='text' name='username' />"
+    username_input = "<input type='text' name='username' value='" + postVars.username + "' />"
+    username_error = style_error(postVars.username_error)
 
     password_label = "<label>Password:</label>"
-    password_input = "<input type='text' name='password' />"
+    password_input = "<input type='password' name='password' value='" + postVars.password + "' />"
+    password_error = style_error(postVars.password_error)
 
     ver_pwd_label = "<label>Verify Password:</label>"
-    ver_pwd_input = "<input type='text' name='ver_pwd' />"
+    ver_pwd_input = "<input type='password' name='ver_pwd' />"
+    ver_pwd_error = style_error(postVars.ver_pwd_error)
 
     email_label = "<label>Email(optional):</label>"
-    email_input = "<input type='text' name='email' />"
+    email_input = "<input type='text' name='email' value='" + postVars.email + "' />"
+    email_error = style_error(postVars.email_error)
 
     submit = "<input type='submit' />"
 
     form = ("<form method='post'>" +
-            username_label + username_input + "<br>" +
-            password_label + password_input + "<br>" +
-            ver_pwd_label + ver_pwd_input + "<br>" +
-            email_label + email_input + "<br>" +
-            submit + "</form>")
+            "<table>" +
+            "<tr>" +
+            "<td>" + username_label + "</td><td>" + username_input + username_error + "</td>" +
+            "</tr><tr>" +
+            "<td>" + password_label + "</td><td>" + password_input + password_error + "</td>" +
+            "</tr><tr>" +
+            "<td>" + ver_pwd_label + "</td><td>" + ver_pwd_input + ver_pwd_error + "</td>" +
+            "</tr><tr>" +
+            "<td>" + email_label + "</td><td>" + email_input + email_error + "</td>" +
+            "</tr>" +
+            "</table>" +
+            submit +
+            "</form>")
 
     header = "<h1>User Signup</h1>"
 
     return header + form
 
+class PostVars:
+    def __init__(self):
+        self.username = ""
+        self.username_error = ""
+        self.password = ""
+        self.password_error = ""
+        self.ver_pwd = ""
+        self.ver_pwd_error = ""
+        self.email = ""
+        self.email_error = ""
 
-class MainHandler(webapp2.RequestHandler):
+class SignupHandler(webapp2.RequestHandler):
     def get(self):
-        content = build_page("")
+        content = build_page(PostVars())
         self.response.write(content)
 
     def post(self):
-        username = self.request.get("username")
-        password = self.request.get("password")
-        ver_pwd = self.request.get("ver_pwd")
-        email = self.request.get("email")
-        content = build_page(escaped_message)
+        USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+        PASSWORD_RE = re.compile(r"^.{3,20}$")
+        EMAIL_RE = re.compile(r"^[\S]+@[\S]+.[\S]+$")
+        form_is_valid = True
+
+        postVars = PostVars()
+        postVars.username = self.request.get("username")
+
+        if not USER_RE.match(postVars.username):
+            postVars.username_error = "Please enter a username between 3 and 20 letters, numbers, underscors, or hyphens."
+            form_is_valid = False
+
+        postVars.password = self.request.get("password")
+
+        if not PASSWORD_RE.match(postVars.password):
+            postVars.password_error = "Passwords must be between 3 and 20 characters."
+            form_is_valid = False
+
+        postVars.ver_pwd = self.request.get("ver_pwd")
+
+        if form_is_valid and postVars.password != postVars.ver_pwd:
+            postVars.ver_pwd_error = "Verify password does not match above password."
+            form_is_valid = False
+
+        postVars.email = self.request.get("email")
+
+        if postVars.email != "" and not EMAIL_RE.match(postVars.email):
+            postVars.email_error = "Please enter a valid email address."
+            form_is_valid = False
+
+        if (form_is_valid):
+            self.redirect("/welcome?name=" + postVars.username)
+        else:
+            content = build_page(postVars)
+            self.response.write(content)
+
+
+class WelcomeHandler(webapp2.RequestHandler):
+    def get(self):
+        username = self.request.get("name")
+        content = "<h1>Welcome, " + username + "!</h1>"
+
         self.response.write(content)
 
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/signup', SignupHandler),
+    ('/welcome', WelcomeHandler)
 ], debug=True)
